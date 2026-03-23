780eb0b139ea39fc109006f24d12f4cc10c36856
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-03-24 01:03:11 +0200

    Add arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,25 @@
 #include "vm.h"
 #include "loader.h"
 
+struct Args {
+    bool no_pause = false;
+    bool debug = false;
+};
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cout << "Enter a file as an argument\n";
         return 0;
+    }
+
+    Args args;
+
+    if (argc > 2) {
+        for (int i = 2; i < argc; i++) {
+            std::string_view arg = argv[i];
+            if (arg == "--no-pause") args.no_pause = true;
+            else if (arg == "--debug") args.debug = true;
+        }
     }
 
     const std::string file = argv[1];
@@ -23,15 +38,16 @@ int main(int argc, char* argv[]) {
     }
 
     VM vm(std::move(p));
-    vm.run();
-
-    if (argc > 2) {
-        std::string_view arg2 = argv[2];
-        if (arg2 == "--no-pause") return 0;
+    try {
+        vm.run();
     }
-    
-    if (std::cin.rdbuf()->in_avail())
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
+    if (args.no_pause)  return 0;
+
     std::cout << "\nPress Enter to exit..." << std::endl;
     std::cin.get();
 
