209da3f5ebb149d46ca9231f7f89a770249b78a8
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-04-09 13:34:35 +0300

    Move functions from random.h to namespace utils

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,24 @@
 #pragma once
 #include <cstring>
 #include <limits>
+#include <random>
 
 namespace utils {
+
+    inline std::random_device                     rd;
+    inline std::mt19937_64                        gen(rd());
+    inline std::uniform_int_distribution<int64_t> dist(
+        std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max()
+    );
+
+    inline int64_t randint() {
+        return dist(gen);
+    }
+
+    inline int64_t randint(const int64_t min, const int64_t max) {
+        std::uniform_int_distribution<int64_t> _dist(min, max);
+        return _dist(gen);
+    }
 
     template <typename T1, typename T2>
     T1 convert(T2 from) {
@@ -21,4 +37,5 @@ namespace utils {
 
         return false;
     }
+
 }
