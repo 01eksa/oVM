4817e4d744f99d5b8854a3c23f9ca61db0b9b80e
Oleksii <114481999+01eksa@users.noreply.github.com>
2026-04-15 17:32:42 +0300

    update config::patch

--- include/config.h
+++ include/config.h
@@ -6,6 +6,6 @@ namespace config {
     constexpr uint32_t magic = 0x3E4D564F;
     constexpr uint16_t major = 0;
     constexpr uint16_t minor = 1;
-    constexpr uint16_t patch = 1;
+    constexpr uint16_t patch = 2;
 
 };
